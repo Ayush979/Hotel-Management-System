0a8c700826f94b662d7a35ad234f5f84f737c334
AYUSH AGRAWAL <52822393+Ayush979@users.noreply.github.com>
2022-04-03 18:31:28 +0530

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,1 +1,19 @@
 
+<!-- footer starts here -->
+    <footer>
+      <hr>
+      <div class="row text-center">
+        
+        <div class="col-md-6 text-center">
+            <ul class="social-icons">
+              Contact Me:
+              <li><a target="_blank" class="github" href="https://github.com/Ayush979"><i class="fab fa-github fa-2x"></i></a></li>
+              
+              <li><a target="_blank" class="linkedin" href="https://www.linkedin.com/in/ayush-agrawal-41421a16b/"><i class="fab fa-linkedin fa-2x"></i></a></li>   
+
+              <li><a target="_blank" class="twitter" href="https://twitter.com/Aayush979"><i class="fab fa-twitter fa-2x"></i></a></li>   
+            </ul>
+        </div>
+      </div>
+    </footer>
+    <!-- footer ends here -->
